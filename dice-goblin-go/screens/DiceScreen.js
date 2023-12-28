c83a98a689a445fe71c2f27b62d2af05b8fe117e
Donte Ladatto <donte279@gmail.com>
2023-12-28 12:32:47 -0500

    create preliminary screens

---
+++ dice-goblin-go/screens/DiceScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import styles from '../styles';
+
+const DiceScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Dice Screen</Text>
+    </View>
+  )
+}
+
+export default DiceScreen
---
+++ dice-goblin-go/screens/StatsScreen.js
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import styles from '../styles';
+
+const StatsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Stats Screen</Text>
+    </View>
+  )
+}
+
+export default StatsScreen
