11092e7ba549b7cce2df7b1e3416bb6e30873c32
Donte Ladatto <donte279@gmail.com>
2023-12-29 21:06:00 -0500

    add status bar back

--- dice-goblin-go/App.js
+++ dice-goblin-go/App.js
@@ -1,10 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { TabNavigator } from './TabNavigator';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
 
   return (
     <NavigationContainer>
+      <StatusBar style="auto" />
       <TabNavigator />
     </NavigationContainer>
   );
