906d025b01dd264f1bcc43a188e6f217e4348753
Donte Ladatto <donte279@gmail.com>
2024-01-17 18:31:20 -0600

    create screen for show die

---
+++ dice-goblin-go/screens/DieShowScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+// Stack navigable from DiceScreen
+
+export const DieShowScreen = () => {
+  return (
+    <View>
+      <Text>DieShowScreen</Text>
+    </View>
+  )
+}
