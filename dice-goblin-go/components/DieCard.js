32790918419cf07cb4d4b0e1697cbecbbd51c91f
Donte Ladatto <donte279@gmail.com>
2024-01-19 17:07:15 -0600

    add component

---
+++ dice-goblin-go/components/DieCard.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DieCard = () => {
+  return (
+    <View>
+      <Text>DieCard</Text>
+    </View>
+  )
+}
+
+export default DieCard
