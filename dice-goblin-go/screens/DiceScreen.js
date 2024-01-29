8dda69ca634d461b0114f11da75bb3d5317774ea
Donte Ladatto <donte279@gmail.com>
2024-01-28 23:00:37 -0600

    add stack nav to dice screen

--- dice-goblin-go/screens/DiceScreen.js
+++ dice-goblin-go/screens/DiceScreen.js
@@ -1,10 +1,14 @@
 import { Text, View } from 'react-native';
 import styles from '../styles';
+import StackNavigator from '../StackNavigator';
+import { useStore } from '../store';
 
 const DiceScreen = () => {
+  const die = useStore(store => store.showDie)
   return (
     <View style={styles.container}>
       <Text>Dice Screen</Text>
+      {die && <StackNavigator name={die.description} />}
     </View>
   )
 }
