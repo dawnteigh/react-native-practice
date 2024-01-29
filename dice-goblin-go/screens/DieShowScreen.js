1a49c3e695339102d2387f19c38006f0fd35a3eb
Donte Ladatto <donte279@gmail.com>
2024-01-28 23:01:07 -0600

    button rendering placeholder

--- dice-goblin-go/screens/DieShowScreen.js
+++ dice-goblin-go/screens/DieShowScreen.js
@@ -1,11 +1,20 @@
 import { Text, View } from 'react-native';
-
-// Stack navigable from DiceScreen
+import { useStore } from '../store';
+import Button from '../components/Button';
 
 export const DieShowScreen = () => {
+
+  const die = useStore(store => store.showDie)
+  const buttons = die.values.map(v => {
+    return (
+      <Button key={v.id} text={v} style="" onClick={() => console.log(`Pressed ${v}`)} />
+    )
+  }
+  )
   return (
     <View>
       <Text>DieShowScreen</Text>
+      {buttons}
     </View>
   )
 }
