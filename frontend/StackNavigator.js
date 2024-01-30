5a4d2dbf741ea09150c4b44a14f47e01da771eb8
Donte Ladatto <donte279@gmail.com>
2024-01-30 15:12:48 -0600

    update import for stack nav

--- frontend/StackNavigator.js
+++ frontend/StackNavigator.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/native-stack";
 import { DieShowScreen } from "./screens/DieShowScreen";
 
 import React from 'react'
