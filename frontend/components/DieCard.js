822e0ebc07da743f7f2be9a32442b7643505112d
Donte Ladatto <donte279@gmail.com>
2024-01-30 16:10:26 -0600

    card render logic

--- frontend/components/DieCard.js
+++ frontend/components/DieCard.js
@@ -1,11 +1,16 @@
-import { View, Text } from 'react-native'
+import { Image, TouchableOpacity, Text } from 'react-native'
 import React from 'react'
+import { useStore } from '../store'
 
-const DieCard = () => {
+const DieCard = ({ die }) => {
+  const setShowDie = useStore(store => store.setShowDie)
+
+  const { id, image_url, description } = die
   return (
-    <View>
-      <Text>DieCard</Text>
-    </View>
+    <TouchableOpacity onPress={() => setShowDie(id)}>
+      <Text>{description}</Text>
+      <Image source={{ uri: image_url }} />
+    </TouchableOpacity>
   )
 }
 
