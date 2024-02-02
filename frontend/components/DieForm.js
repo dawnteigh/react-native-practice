6db66f60c97aa9daca847c350a98c42e10caab39
Donte Ladatto <donte279@gmail.com>
2024-02-01 20:48:08 -0600

    add form component

---
+++ frontend/components/DieForm.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DieForm = () => {
+  return (
+    <View>
+      <Text>DieForm</Text>
+    </View>
+  )
+}
+
+export default DieForm
