cf7d47de023823d82a3891734105eb8a6b281987
Donte Ladatto <donte279@gmail.com>
2024-02-01 20:48:18 -0600

    add toolbar component

---
+++ frontend/components/Toolbar.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Toolbar = () => {
+  return (
+    <View>
+      <Text>Toolbar</Text>
+    </View>
+  )
+}
+
+export default Toolbar
