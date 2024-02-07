85eea45b619bd01b89cd9806c71374b5420b4d8b
Donte Ladatto <donte279@gmail.com>
2024-02-06 22:48:00 -0600

    add image picker component

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,10 +1,12 @@
 import { Text, View } from 'react-native';
 import styles from '../styles';
+import ImagePickerExample from '../components/ImagePicker';
 
 const HomeScreen = () => {
   return (
     <View style={styles.container} >
       <Text>Home Screen</Text>
+      <ImagePickerExample />
     </View>
   )
 }
