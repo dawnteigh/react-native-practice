3b5c446fcdc4d1f0a312f341668c31edc43f1642
Donte Ladatto <donte279@gmail.com>
2024-02-12 21:00:28 -0600

    open db and migrations logic

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,32 @@
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { TabNavigator } from './TabNavigator';
 import { StatusBar } from 'expo-status-bar';
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { openDatabaseSync } from "expo-sqlite/next";
+import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
+import migrations from './drizzle/migrations';
+
+const expoDb = openDatabaseSync("dg.db");
+const db = drizzle(expoDb);
 
 export default function App() {
+
+  const { success, error } = useMigrations(db, migrations);
+  if (error) {
+    return (
+      <View>
+        <Text>Migration error: {error.message}</Text>
+      </View>
+    );
+  }
+  if (!success) {
+    return (
+      <View>
+        <Text>Migration is in progress...</Text>
+      </View>
+    );
+  }
 
   return (
     <NavigationContainer>
