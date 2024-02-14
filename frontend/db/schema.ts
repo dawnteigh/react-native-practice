696c43a9388bebd497d90a367169c41d31af77ce
Donte Ladatto <donte279@gmail.com>
2024-02-13 21:18:41 -0600

    add relations to schema

--- frontend/db/schema.ts
+++ frontend/db/schema.ts
@@ -1,4 +1,6 @@
 import { sqliteTable, text, integer, real } from 'drizzle-orm/sqlite-core';
+import { relations } from 'drizzle-orm';
+
 export const dice = sqliteTable('dice', {
   id: integer('id').primaryKey(),
   description: text('description'),
@@ -8,9 +10,21 @@ export const dice = sqliteTable('dice', {
   totalRolls: integer('totalRolls').default(0),
   averageRoll: real('averageRoll').default(0.0)
 });
+
+export const diceRelations = relations(dice, ({ many }) => ({
+  values: many(values),
+}));
+
 export const values = sqliteTable('values', {
   id: integer('id').primaryKey(),
   value: integer('value'),
   dieId: integer('dieId').references(() => dice.id),
   timesRolled: integer('timesRolled').default(0)
-})
+});
+
+export const valuesRelations = relations(values, ({ one }) => ({
+  die: one(dice, {
+    fields: [values.dieId],
+    references: [dice.id],
+  }),
+}));
