ba231af3ea38baafac07b38c801ef9ae1162e158
Donte Ladatto <donte279@gmail.com>
2024-02-18 23:13:23 -0600

    determine numOfValues w/ dieType

--- frontend/models/Die.ts
+++ frontend/models/Die.ts
@@ -9,13 +9,23 @@ export class Die {
   averageRoll: number;
   values: Value[];
 
-  constructor(description: string, dieType: string, image: string, numOfValues: number) {
+  constructor(description: string, dieType: string, image: string) {
     this.description = description;
     this.dieType = dieType;
     this.image = image;
-    this.numOfValues = numOfValues;
+    this.numOfValues = this.findNumOfValues(dieType);
     this.totalRolls = 0;
     this.averageRoll = 0;
     // create value objects for the type of die
+  }
+
+  findNumOfValues(type: string): number {
+    if (type === 'd%') return 10;
+    if (type === '2d6') return 11;
+    else return parseInt(type)
+  }
+
+  createValues(type: string): void {
+
   }
 }
