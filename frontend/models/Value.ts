48092821080d41a23f4d82d86e568fd86bbfe305
Donte Ladatto <donte279@gmail.com>
2024-02-19 21:42:02 -0600

    value constructor

--- frontend/models/Value.ts
+++ frontend/models/Value.ts
@@ -1,4 +1,11 @@
 export class Value {
   value: number;
+  dieId: number;
   timesRolled: number;
+
+  constructor(value: number, dieId: number) {
+    this.value = value;
+    this.dieId = dieId;
+    this.timesRolled = 0;
+  }
 }
